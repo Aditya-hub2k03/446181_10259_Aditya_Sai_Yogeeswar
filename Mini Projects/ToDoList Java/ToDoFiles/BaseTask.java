e17b381766c73fb9d9374f3961520b0f2cd8bfe9
Aditya_2K03 <85829277+Aditya-hub2k03@users.noreply.github.com>
2025-08-06 21:35:28 +0530

    Update BaseTask.java

--- Mini Projects/ToDoList Java/ToDoFiles/BaseTask.java
+++ Mini Projects/ToDoList Java/ToDoFiles/BaseTask.java
@@ -1,29 +1,20 @@
 package com.aditya.todo;
 
-import java.time.LocalDateTime;
+import java.util.Date;
 
 public class BaseTask {
     int id;
     String description;
-    LocalDateTime targetDateTime;
+    Date addedDate;
+    Date editedDate;
+    Date targetDateTime;
     String status;
 
-    public BaseTask(int id, String description, LocalDateTime targetDateTime) {
+    public BaseTask(int id, String description, Date addedDate, Date targetDateTime, String status) {
         this.id = id;
         this.description = description;
+        this.addedDate = addedDate;
         this.targetDateTime = targetDateTime;
-        this.status = "Pending";
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setTargetDateTime(LocalDateTime targetDateTime) {
-        this.targetDateTime = targetDateTime;
-    }
-
-    public void setStatus(String status) {
         this.status = status;
     }
 }
