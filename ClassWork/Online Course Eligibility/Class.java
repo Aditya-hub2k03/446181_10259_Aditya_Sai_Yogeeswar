c47ac20de826fc0a8fb2ec7e96143ac0f433039b
Aditya_2K03 <85829277+Aditya-hub2k03@users.noreply.github.com>
2025-08-08 14:50:07 +0530

    Create Class.java

---
+++ ClassWork/Online Course Eligibility/Class.java
@@ -0,0 +1,31 @@
+package com.yogesh.courseeligibility;
+
+public class Course {
+    private String courseName;
+    private String instructor;
+    private int credits;
+
+    public Course(String courseName, String instructor, int credits) {
+        this.courseName = courseName;
+        this.instructor = instructor;
+        this.credits = credits;
+    }
+
+    public void displayCourseDetails() {
+        System.out.println("Course Name: " + courseName);
+        System.out.println("Instructor: " + instructor);
+        System.out.println("Credits: " + credits);
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+}
