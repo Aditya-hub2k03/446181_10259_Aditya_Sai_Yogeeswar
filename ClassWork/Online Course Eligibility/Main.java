4ef5ca4b15f44aabf8acab0214d2abc0884a32c6
Aditya_2K03 <85829277+Aditya-hub2k03@users.noreply.github.com>
2025-08-08 14:50:41 +0530

    Create Main.java

---
+++ ClassWork/Online Course Eligibility/Main.java
@@ -0,0 +1,15 @@
+package com.yogesh.courseeligibility;
+
+public class Main {
+    public static void main(String[] args) {
+        OnlineCourse onlineCourse = new OnlineCourse("Introduction to Java Programming", "ASYP", 3, "TMF", 12);
+
+        onlineCourse.displayCourseDetails();
+
+        if (onlineCourse.isEligibleForCertificate()) {
+            System.out.println("This course is eligible for a certificate.");
+        } else {
+            System.out.println("This course is not eligible for a certificate.");
+        }
+    }
+}
