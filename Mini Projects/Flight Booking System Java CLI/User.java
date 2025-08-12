6fa914a7e8dcc9cfb406842c3dd03f4d3243aea6
Aditya_2K03 <85829277+Aditya-hub2k03@users.noreply.github.com>
2025-08-12 14:58:43 +0530

    Create User.java

---
+++ Mini Projects/Flight Booking System Java CLI/User.java
@@ -0,0 +1,12 @@
+package com.yogesh.flightbookingsystem;
+
+abstract public class User {
+	protected String name;
+	public User(String name) {
+		this.name = name;
+		
+	}
+	
+	public abstract void showMenu();
+	
+}
