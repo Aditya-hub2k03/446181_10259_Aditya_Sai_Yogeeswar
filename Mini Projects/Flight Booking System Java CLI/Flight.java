d554d8a0f88ca7d2be9069a3be9e9882301fc3cf
Aditya_2K03 <85829277+Aditya-hub2k03@users.noreply.github.com>
2025-08-16 22:42:58 +0530

    Update Flight.java

--- Mini Projects/Flight Booking System Java CLI/Flight.java
+++ Mini Projects/Flight Booking System Java CLI/Flight.java
@@ -9,7 +9,7 @@ public class Flight {
 	public Flight(String flightNo, String source, String destination, double price) {
 		this.flightNo = flightNo;
 		this.source = source;
-		this.destination = destination;
+		this.destination = destination; 
 		this.price = price;
 		
 	}
