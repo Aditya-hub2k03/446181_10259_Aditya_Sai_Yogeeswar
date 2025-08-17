ce880b7be033b28368d2cb2e20a758be0efbdfda
Aditya_2K03 <yogeeswarp@gmail.com>
2025-08-17 19:01:00 +0530

    Update Admin.java

--- Mini Projects/Restaurant Management System/Admin.java
+++ Mini Projects/Restaurant Management System/Admin.java
@@ -1,97 +1,75 @@
 package com.yogesh.rmsv1;
-
 import java.util.Scanner;
 
 public class Admin {
-    private static final String ADMIN_ID = "admin";
-    private static final String ADMIN_PASS = "password";
-    private static final Scanner sc = new Scanner(System.in);
+    private String adminId = "admin";
+    private String password = "password";
+    private Menu menu;
 
-    public static void login() {
-        System.out.print("Enter Admin ID: ");
-        String id = sc.next();
-        System.out.print("Enter Password: ");
-        String pass = sc.next();
-
-        if (id.equals(ADMIN_ID) && pass.equals(ADMIN_PASS)) {
-            System.out.println("Login Successful!");
-            adminMenu();
-        } else {
-            System.out.println("Invalid Credentials!");
-        }
+    public Admin(Menu menu) {
+        this.menu = menu;
     }
 
-    private static void adminMenu() {
-        while (true) {
-            System.out.println("\n====== Admin Menu ======");
+    public boolean login(Scanner sc) {
+        System.out.print("Enter Admin ID: ");
+        String id = sc.nextLine();
+        System.out.print("Enter Password: ");
+        String pass = sc.nextLine();
+        return id.equals(adminId) && pass.equals(password);
+    }
+
+    public void adminMenu(Scanner sc) {
+        int choice;
+        do {
+            System.out.println("\n--- Admin Menu ---");
             System.out.println("1. View Menu");
-            System.out.println("2. Add Item");
-            System.out.println("3. Update Item");
-            System.out.println("4. Delete Item");
-            System.out.println("5. Load Menu from File");
-            System.out.println("6. Exit");
-            System.out.print("Enter choice: ");
-            int choice = sc.nextInt();
+            System.out.println("2. Add an Item");
+            System.out.println("3. Update an Item");
+            System.out.println("4. Delete an Item");
+            System.out.println("5. Load Menu from Path");
+            System.out.println("6. Exit (Save Menu)");
+            System.out.print("Enter your choice: ");
+            choice = Integer.parseInt(sc.nextLine());
 
             switch (choice) {
                 case 1:
-                    Menu.showMenu();
+                    menu.displayMenu();
                     break;
                 case 2:
-                    addItem();
+                    System.out.print("Enter Item ID: ");
+                    int id = Integer.parseInt(sc.nextLine());
+                    System.out.print("Enter Item Name: ");
+                    String name = sc.nextLine();
+                    System.out.print("Enter Item Price: ");
+                    String price = sc.nextLine();
+                    menu.addItem(id, name, price);
                     break;
                 case 3:
-                    updateItem();
+                    System.out.print("Enter Item ID to update: ");
+                    int uid = Integer.parseInt(sc.nextLine());
+                    System.out.print("Enter New Name: ");
+                    String newName = sc.nextLine();
+                    System.out.print("Enter New Price: ");
+                    String newPrice = sc.nextLine();
+                    menu.updateItem(uid, newName, newPrice);
                     break;
                 case 4:
-                    deleteItem();
+                    System.out.print("Enter Item ID to delete: ");
+                    int did = Integer.parseInt(sc.nextLine());
+                    menu.deleteItem(did);
                     break;
                 case 5:
-                    loadMenuFromFile();
+                    System.out.print("Enter file path to load menu: ");
+                    String path = sc.nextLine();
+                    menu.loadMenuFromFile(path);
                     break;
                 case 6:
-                    return;
+                    menu.saveMenuToFile();
+                    System.out.println("Exiting Admin...");
+                    break;
                 default:
                     System.out.println("Invalid choice!");
             }
-        }
-    }
-
-    private static void addItem() {
-        System.out.print("Enter Item ID: ");
-        int id = sc.nextInt();
-        sc.nextLine(); 
-        System.out.print("Enter Item Name: ");
-        String name = sc.nextLine();
-        System.out.print("Enter Item Price: ");
-        double price = sc.nextDouble();
-
-        Menu.addItem(id, name, price);
-        System.out.println("Item added successfully!");
-    }
-
-    private static void updateItem() {
-        System.out.print("Enter Item ID to update: ");
-        int id = sc.nextInt();
-        sc.nextLine();
-        System.out.print("Enter New Name: ");
-        String name = sc.nextLine();
-        System.out.print("Enter New Price: ");
-        double price = sc.nextDouble();
-
-        Menu.updateItem(id, name, price);
-    }
-
-    private static void deleteItem() {
-        System.out.print("Enter Item ID to delete: ");
-        int id = sc.nextInt();
-        Menu.deleteItem(id);
-    }
-
-    private static void loadMenuFromFile() {
-        sc.nextLine();
-        System.out.print("Enter file path: ");
-        String path = sc.nextLine();
-        Menu.loadMenuFromFile(path);
+        } while (choice != 6);
     }
 }
