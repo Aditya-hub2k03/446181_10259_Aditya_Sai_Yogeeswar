14735b93b494dc4611181a527463526e0ecd5b61
Aditya_2K03 <yogeeswarp@gmail.com>
2025-08-17 19:00:19 +0530

    Update MenuItem.java

--- Mini Projects/Restaurant Management System/MenuItem.java
+++ Mini Projects/Restaurant Management System/MenuItem.java
@@ -1,13 +1,25 @@
 package com.yogesh.rmsv1;
 
 public class MenuItem {
-    int id;
-    String name;
-    double price;
+    private int id;
+    private String name;
+    private double price;
 
     public MenuItem(int id, String name, double price) {
         this.id = id;
         this.name = name;
         this.price = price;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+
+    public void setName(String name) { this.name = name; }
+    public void setPrice(double price) { this.price = price; }
+
+    @Override
+    public String toString() {
+        return String.format("# %-7d # %-20s # %-10.2f #", id, name, price);
     }
 }
