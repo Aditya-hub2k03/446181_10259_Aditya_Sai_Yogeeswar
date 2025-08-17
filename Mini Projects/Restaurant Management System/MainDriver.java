f234b81e04c22d7846e60168fc8b5310e73dd960
Aditya_2K03 <yogeeswarp@gmail.com>
2025-08-17 19:01:42 +0530

    Update MainDriver.java

--- Mini Projects/Restaurant Management System/MainDriver.java
+++ Mini Projects/Restaurant Management System/MainDriver.java
@@ -1,33 +1,41 @@
 package com.yogesh.rmsv1;
-
 import java.util.Scanner;
 
 public class MainDriver {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Menu menu = new Menu();
 
-        while (true) {
-            System.out.println("\n====== Restaurant Management System ======");
+        int choice;
+        do {
+            System.out.println("\n--- Welcome to Restaurant Management System ---");
             System.out.println("1. Customer");
             System.out.println("2. Admin");
             System.out.println("3. Exit");
-            System.out.print("Choose your role: ");
-            int choice = sc.nextInt();
+            System.out.print("Enter your choice: ");
+            choice = Integer.parseInt(sc.nextLine());
 
             switch (choice) {
                 case 1:
-                    Customer.customerMenu();
+                    Customer cust = new Customer(menu);
+                    cust.customerMenu(sc);
                     break;
                 case 2:
-                    Admin.login();
+                    Admin admin = new Admin(menu);
+                    if (admin.login(sc)) {
+                        admin.adminMenu(sc);
+                    } else {
+                        System.out.println("Invalid login!");
+                    }
                     break;
                 case 3:
-                    System.out.println("Exiting... Thank you!");
-                    sc.close();
-                    return;
+                    System.out.println("Thank you for using Restaurant Management System!");
+                    break;
                 default:
-                    System.out.println("Invalid choice! Try again.");
+                    System.out.println("Invalid choice!");
             }
-        }
+        } while (choice != 3);
+
+        sc.close();
     }
 }
