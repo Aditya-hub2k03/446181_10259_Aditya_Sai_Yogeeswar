a1a2b29540942dd2420ae4692df16aedc6434f37
Aditya_2K03 <yogeeswarp@gmail.com>
2025-08-17 18:20:58 +0530

    Update MenuItem.java

---
+++ Mini Projects/Restaurant Management System/MenuItem.java
@@ -0,0 +1,13 @@
+package com.yogesh.rmsv1;
+
+public class MenuItem {
+    int id;
+    String name;
+    double price;
+
+    public MenuItem(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+}
