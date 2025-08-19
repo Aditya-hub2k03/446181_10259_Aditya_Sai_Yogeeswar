9db4e985ebfcfc1d62efe8dd2a629ebec4bfffe9
Aditya_2K03 <85829277+Aditya-hub2k03@users.noreply.github.com>
2025-08-19 09:59:53 +0530

    Create DataLoader.java

---
+++ Mini Projects/Happy Mart/DataLoader.java
@@ -0,0 +1,24 @@
+package com.yogesh.happymart;
+import java.io.*;
+import java.util.*;
+
+public class DataLoader {
+    public static Map<Integer, Items> loadItems(String filePath) {
+        Map<Integer, Items> map = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while((line = br.readLine()) != null) {
+                String[] parts = line.split(",");
+                int id = Integer.parseInt(parts[0]);
+                String name = parts[1];
+                String brand = parts[2];
+                String property = parts[3];
+                int price = Integer.parseInt(parts[4]);
+                map.put(id, new Items(id, name, brand, property, price));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return map;
+    }
+}
