68fa0d4090fbafccbf6675d5d6cdcfdee74c74ae
Aditya_2K03 <85829277+Aditya-hub2k03@users.noreply.github.com>
2025-08-28 16:19:59 +0530

    Create DeleteUserServlet.java

---
+++ ClassWork/servlet_reg_log_user/Servlet Files/DeleteUserServlet.java
@@ -0,0 +1,27 @@
+package com.yogesh.reg;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.*;
+import java.sql.*;
+//import jakarta.servlet.annotation.WebServlet;
+//@WebServlet("/register")
+public class DeleteUserServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String id = request.getParameter("id");
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/userdb", "root", "root");
+
+            PreparedStatement ps = con.prepareStatement("DELETE FROM users WHERE id=?");
+            ps.setInt(1, Integer.parseInt(id));
+            ps.executeUpdate();
+            con.close();
+
+            response.sendRedirect("viewUsers");
+        } catch (Exception e) {
+            response.getWriter().println("<h3>Error: " + e.getMessage() + "</h3>");
+        }
+    }
+}
