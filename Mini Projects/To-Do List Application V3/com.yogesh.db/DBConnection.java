91ad60e88cd569408e19389dd9c499da31880644
Aditya_2K03 <85829277+Aditya-hub2k03@users.noreply.github.com>
2025-09-01 15:19:12 +0530

    Create DBConnection.java

---
+++ Mini Projects/To-Do List Application V3/com.yogesh.db/DBConnection.java
@@ -0,0 +1,16 @@
+package com.yogesh.db;
+import java.sql.*;
+
+
+public class DBConnection {
+	private static final String URL="jdbc:mysql://localhost:3306/tododb";
+	private static final String USER="root";
+	private static final String PASS="root";
+	
+	public static Connection getConnection() throws Exception{
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		return DriverManager.getConnection(URL,USER,PASS);
+	}
+	
+
+}
