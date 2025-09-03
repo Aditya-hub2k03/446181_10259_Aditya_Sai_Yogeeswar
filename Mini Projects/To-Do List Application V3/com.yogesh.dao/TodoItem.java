39525f8429b563da2cc03165032c54c76dfa7639
Aditya_2K03 <85829277+Aditya-hub2k03@users.noreply.github.com>
2025-09-03 15:15:07 +0530

    Update TodoItem.java

--- Mini Projects/To-Do List Application V3/com.yogesh.dao/TodoItem.java
+++ Mini Projects/To-Do List Application V3/com.yogesh.dao/TodoItem.java
@@ -7,7 +7,8 @@ public class TodoItem {
     private String todoTitle;
     private String todoDesc;
     private LocalDateTime targetDatetime;
-    private String todoStatusCode;   // 'P', 'I', 'C'
+    private String todoStatusCode;   
+    private int userId;              
     private String createdBy;
     private LocalDateTime createdDate;
     private String modifiedBy;
@@ -17,54 +18,79 @@ public class TodoItem {
     public int getTodoId() {
         return todoId;
     }
+
     public void setTodoId(int todoId) {
         this.todoId = todoId;
     }
+
     public String getTodoTitle() {
         return todoTitle;
     }
+
     public void setTodoTitle(String todoTitle) {
         this.todoTitle = todoTitle;
     }
+
     public String getTodoDesc() {
         return todoDesc;
     }
+
     public void setTodoDesc(String todoDesc) {
         this.todoDesc = todoDesc;
     }
+
     public LocalDateTime getTargetDatetime() {
         return targetDatetime;
     }
+
     public void setTargetDatetime(LocalDateTime targetDatetime) {
         this.targetDatetime = targetDatetime;
     }
+
     public String getTodoStatusCode() {
         return todoStatusCode;
     }
+
     public void setTodoStatusCode(String todoStatusCode) {
         this.todoStatusCode = todoStatusCode;
     }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
     public String getCreatedBy() {
         return createdBy;
     }
+
     public void setCreatedBy(String createdBy) {
         this.createdBy = createdBy;
     }
+
     public LocalDateTime getCreatedDate() {
         return createdDate;
     }
+
     public void setCreatedDate(LocalDateTime createdDate) {
         this.createdDate = createdDate;
     }
+
     public String getModifiedBy() {
         return modifiedBy;
     }
+
     public void setModifiedBy(String modifiedBy) {
         this.modifiedBy = modifiedBy;
     }
+
     public LocalDateTime getModifiedDate() {
         return modifiedDate;
     }
+
     public void setModifiedDate(LocalDateTime modifiedDate) {
         this.modifiedDate = modifiedDate;
     }
