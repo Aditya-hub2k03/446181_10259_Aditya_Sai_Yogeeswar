310b66dd7b75e87e89e9e89adb6e755664231d94
Aditya_2K03 <85829277+Aditya-hub2k03@users.noreply.github.com>
2025-09-08 22:30:00 +0530

    Create User.java

---
+++ ClassWork/Mixed Configuration Spring Files/src_main_java/com.yp.pro1/User.java
@@ -0,0 +1,26 @@
+package com.yp.pro1;
+
+public class User {
+    private String name;
+    private int id;
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", name=" + name + "]";
+    }
+}
