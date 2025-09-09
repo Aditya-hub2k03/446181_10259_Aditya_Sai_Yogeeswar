9ee50aa09831918d3721d58a5bf4cbda63bd478c
Aditya_2K03 <85829277+Aditya-hub2k03@users.noreply.github.com>
2025-09-09 16:24:10 +0530

    Create AppConfig.java

---
+++ ClassWork/Pro1/src_main_java/com.yogesh.annobased/AppConfig.java
@@ -0,0 +1,10 @@
+package com.yogesh.annobased;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.yogesh.annobased")
+public class AppConfig {
+   
+}
