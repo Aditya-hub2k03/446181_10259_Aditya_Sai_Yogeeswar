96ec730c1bb2ff065598c814b9f9ac83fe49a257
Aditya_2K03 <85829277+Aditya-hub2k03@users.noreply.github.com>
2025-09-09 16:24:54 +0530

    Create Address.java

---
+++ ClassWork/Pro1/src_main_java/com.yogesh.javabased/Address.java
@@ -0,0 +1,29 @@
+package com.yogesh.javabased;
+
+
+public class Address {
+    private String city;
+    private String state;
+
+    // Getters and setters
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return city + ", " + state;
+    }
+}
