8ada6b01f24149b1a54dc1da45dddb68e1dd8e8f
Aditya_2K03 <85829277+Aditya-hub2k03@users.noreply.github.com>
2025-09-09 23:31:54 +0530

    Create Address.java

---
+++ ClassWork/Pro1/src_main_java/com.xmldi.cisimix/Address.java
@@ -0,0 +1,33 @@
+package com.xmldi.cisimix;
+
+public class Address {
+
+    private String city;
+    private String state;
+
+    public Address() {
+    }
+
+    public Address(String city, String state) {
+        this.city = city;
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getState() {
+        return state;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return city + ", " + state;
+    }
+}
