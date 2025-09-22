781e1d6d57ca49e6d01a5b57a62f5d2c90b9030b
Aditya_2K03 <85829277+Aditya-hub2k03@users.noreply.github.com>
2025-09-22 16:25:42 +0530

    Update DataLoader.java

--- Mini Projects/Happy Mart/DataLoader.java
+++ Mini Projects/Happy Mart/DataLoader.java
@@ -6,7 +6,7 @@ public class DataLoader {
     public static Map<Integer, Items> loadItems(String filePath) {
         Map<Integer, Items> map = new HashMap<>();
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            String line;
+            String line; 
             while((line = br.readLine()) != null) {
                 String[] parts = line.split(",");
                 int id = Integer.parseInt(parts[0]);
