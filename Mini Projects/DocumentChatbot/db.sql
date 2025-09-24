29c8cc185b32858593e230c5d201345b69adbc86
Aditya_2K03 <85829277+Aditya-hub2k03@users.noreply.github.com>
2025-09-24 22:06:04 +0530

    Create db.sql

---
+++ Mini Projects/DocumentChatbot/db.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE chatbot_db;
+
+USE chatbot_db;
+
+CREATE TABLE chat_messages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    message TEXT,
+    response TEXT,
+    model VARCHAR(50)
+);
+
+select * form chat_messages;
+
+CREATE TABLE documents (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    content LONGTEXT,
+    type VARCHAR(100)
+);
+
+select * from documents;
