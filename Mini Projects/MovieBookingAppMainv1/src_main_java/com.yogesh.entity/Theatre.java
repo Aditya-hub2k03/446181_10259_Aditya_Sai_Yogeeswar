c503bd95bdcbd5eed66846977b75467d65f8e705
Aditya_2K03 <85829277+Aditya-hub2k03@users.noreply.github.com>
2025-10-06 12:24:13 +0530

    Create Theatre.java

---
+++ Mini Projects/MovieBookingAppMainv1/src_main_java/com.yogesh.entity/Theatre.java
@@ -0,0 +1,11 @@
+package com.yogesh.entity;
+
+import lombok.Data;
+
+@Data
+public class Theatre {
+    private int theatreId;
+    private String theatreName;
+    private String city;
+    private String address;
+}
